69e9c23bd48580f3733279600d1372ea3557fea9
claudia <claudia.iacob@hotmail.com>
2023-07-04 19:06:32 +0200

    First commit

---
+++ js/mijs.js
@@ -0,0 +1,27 @@
+// const songList = [
+//   {
+//     title: "Bobby McFerrin - Don't Worry Be Happy",
+//     file: "behappy.mp3",
+//     cover: ""
+//   },
+//   {
+//     title: "Johannes Brahms - Hungarian Dance",
+//     file: "hungariandance.mp3",
+//     cover: ""
+//   },
+//   {
+//     title: "Dukas - The Sorcerer’s Apprentice",
+//     file: "dukas.mp3",
+//     cover: ""
+//   },
+//   {
+//     title: "James Brown - I Feel Good",
+//     file: "feelgood.mp3",
+//     cover: ""
+//   },
+//   {
+//     title: "Pablo Sarasate - Gypsy Airs",
+//     file: "gypsyairs.mp3",
+//     cover: ""
+//   }
+// ]
