6d13e050193ea2bdca5ea9e407fd5420ae821d78
claudia <claudia.iacob@hotmail.com>
2023-07-04 19:50:40 +0200

    Listado terminado

--- js/mijs.js
+++ js/mijs.js
@@ -1,27 +1,27 @@
-// const songList = [
-//   {
-//     title: "Bobby McFerrin - Don't Worry Be Happy",
-//     file: "behappy.mp3",
-//     cover: ""
-//   },
-//   {
-//     title: "Johannes Brahms - Hungarian Dance",
-//     file: "hungariandance.mp3",
-//     cover: ""
-//   },
-//   {
-//     title: "Dukas - The Sorcerer’s Apprentice",
-//     file: "dukas.mp3",
-//     cover: ""
-//   },
-//   {
-//     title: "James Brown - I Feel Good",
-//     file: "feelgood.mp3",
-//     cover: ""
-//   },
-//   {
-//     title: "Pablo Sarasate - Gypsy Airs",
-//     file: "gypsyairs.mp3",
-//     cover: ""
-//   }
-// ]
+const songList = [
+  {
+    title: "Bobby McFerrin - Don't Worry Be Happy",
+    file: "behappy.mp3",
+    cover: ""
+  },
+  {
+    title: "Johannes Brahms - Hungarian Dance",
+    file: "hungariandance.mp3",
+    cover: ""
+  },
+  {
+    title: "Dukas - The Sorcerer’s Apprentice",
+    file: "dukas.mp3",
+    cover: ""
+  },
+  {
+    title: "James Brown - I Feel Good",
+    file: "feelgood.mp3",
+    cover: ""
+  },
+  {
+    title: "Pablo Sarasate - Gypsy Airs",
+    file: "gypsyairs.mp3",
+    cover: ""
+  }
+]
